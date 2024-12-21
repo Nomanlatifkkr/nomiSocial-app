e430f5b4027fd571edde6a6291f35e1f81a4fcb1
Nomanlatifkkr <mahnoorkkr786@gmail.com>
2024-12-21 11:42:37 +0500

    Create cloudinary.js

---
+++ server/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary').v2; // Import Cloudinary SDK
+require('dotenv').config(); // Load environment variables
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = cloudinary; // Export for use in other files
